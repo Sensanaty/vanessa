0f2510f5241b5f1514a1d1bc7bde3850848c120b
Luka Salevic <salevic@lu-ka.me>
2020-07-29 21:11:14 +0200

    Create the website footer

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,24 @@
 <template>
     <div id="app">
         <router-view />
+        <Footer />
     </div>
 </template>
 
-<style lang="scss"></style>
+<style lang="scss">
+    #app {
+        display: flex;
+        flex-flow: column nowrap;
+    }
+</style>
+
+<script>
+    import Footer from "@/components/Footer";
+
+    export default {
+        name: "App",
+        components: {
+            Footer
+        }
+    };
+</script>
