248280d9e8f3172ff5ceb647869c08352179d414
Luka Salevic <salevic@lu-ka.me>
2020-07-29 17:56:36 +0200

    Remove initial vue-cli setup structure and setup basic routes

--- src/router/index.js
+++ src/router/index.js
@@ -1,30 +1,49 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
-import Home from "../views/Home.vue";
+import Home from "@/views/Home.vue";
 
 Vue.use(VueRouter);
 
 const routes = [
-  {
-    path: "/",
-    name: "Home",
-    component: Home
-  },
-  {
-    path: "/about",
-    name: "About",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/About.vue")
-  }
+    {
+        path: "/",
+        name: "Home",
+        component: Home
+    },
+    {
+        path: "/projects",
+        name: "Projects",
+        component: () => {
+            import(/* webpackChunkName: "projects" */ "@/views/Projects.vue");
+        }
+    },
+    {
+        path: "/gallery",
+        name: "Gallery",
+        component: () => {
+            import(/* webpackChunkName: "gallery" */ "@/views/Gallery.vue");
+        }
+    },
+    {
+        path: "/about",
+        name: "About",
+        component: () => {
+            import(/* webpackChunkName: "about" */ "@/views/About.vue");
+        }
+    },
+    {
+        path: "/contact",
+        name: "Contact",
+        component: () => {
+            import(/* webpackChunkName: "contact" */ "@/views/Contact.vue");
+        }
+    }
 ];
 
 const router = new VueRouter({
-  mode: "history",
-  base: process.env.BASE_URL,
-  routes
+    mode: "history",
+    base: process.env.BASE_URL,
+    routes
 });
 
 export default router;
