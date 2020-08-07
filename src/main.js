fddf5d0f31d41cc57a61b6b2819ac6037ca69396
Luka Salevic <salevic@lu-ka.me>
2020-08-07 12:50:18 +0200

    Add vue-lazyload to root file

--- src/main.js
+++ src/main.js
@@ -1,8 +1,13 @@
 import Vue from "vue";
 import App from "@/App.vue";
 import router from "@/router";
+import VueLazyload from "vue-lazyload";
 import "@/styles/normalize.css";
 import "@/styles/main.scss";
+
+Vue.use(VueLazyload, {
+    loading: "images/ohmlaut.svg"
+});
 
 Vue.config.productionTip = false;
 
