24d70d1ff20fdd717b49a814f402b6fff7d8fb8f
Luka Salevic <salevic@lu-ka.me>
2020-08-11 23:53:50 +0200

    Fix ESLint problems

--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@ import Vue from "vue";
 import App from "@/App.vue";
 import router from "@/router";
 import "overlayscrollbars/css/OverlayScrollbars.css";
-import OverlayScrollbars from 'overlayscrollbars';
+import OverlayScrollbars from "overlayscrollbars";
 import { OverlayScrollbarsPlugin } from "overlayscrollbars-vue";
 import "@/styles/normalize.css";
 import "@/styles/main.scss";
