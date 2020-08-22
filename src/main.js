6a9ca25be026bff13bdd91e08de55926e04e7f10
Sensanaty <salevic@lu-ka.me>
2020-08-22 11:47:57 +0200

    Add vue-meta package to project and configure it in main.js

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import Vue from "vue";
 import App from "@/App.vue";
 import router from "@/router";
+import VueMeta from "vue-meta";
 import "overlayscrollbars/css/OverlayScrollbars.css";
 import OverlayScrollbars from "overlayscrollbars";
 import { OverlayScrollbarsPlugin } from "overlayscrollbars-vue";
@@ -10,6 +11,7 @@ import "@/styles/main.scss";
 Vue.config.productionTip = false;
 
 Vue.use(OverlayScrollbarsPlugin);
+Vue.use(VueMeta);
 
 new Vue({
     router,
