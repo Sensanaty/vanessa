a7d18520eeb0d050c0e5fc09efaaa3ec9fdf5d2e
Sensanaty <salevic@lu-ka.me>
2020-08-22 13:59:32 +0200

    Finish the look of the main Gallery View and the GalleryPage view

---
+++ src/components/GalleryCard.vue
@@ -0,0 +1,78 @@
+<template>
+    <router-link class="gallery-link" :to="{ name: 'GalleryPage', params: { id: url } }" exact>
+        <div class="gallery-card">
+            <picture>
+                <source :srcset="`${image}.webp`" type="image/webp" />
+                <source :srcset="`${image}.jpg`" type="image/jpeg" />
+                <img :src="`${image}.jpg`" :alt="title" class="gallery-image" />
+            </picture>
+
+            <div class="text-wrapper">
+                <h1 class="gallery-title">{{ title.toUpperCase() }}</h1>
+            </div>
+        </div>
+    </router-link>
+</template>
+
+<script>
+    export default {
+        name: "GalleryCard",
+        props: {
+            image: String,
+            altText: String,
+            title: String,
+            url: String
+        }
+    };
+</script>
+
+<style lang="scss" scoped>
+    .gallery-card {
+        height: 650px;
+        width: 650px;
+        position: relative;
+        margin: 30px 0;
+        background-image: url("/images/ohmlaut.svg");
+
+        .gallery-title {
+            text-align: center;
+            transition: all 5ms ease-in-out;
+            opacity: 0;
+            font-size: 4rem;
+        }
+
+        .gallery-image {
+            height: 100%;
+            width: 100%;
+            object-fit: fill;
+            transition: all 150ms ease-in-out;
+            user-select: none;
+        }
+
+        .text-wrapper {
+            display: flex;
+            flex-flow: column wrap;
+            justify-content: center;
+            align-items: center;
+            width: 100%;
+            height: 100%;
+            user-select: none;
+            position: absolute;
+            bottom: 0;
+            left: 0;
+            opacity: 0;
+            background: transparentize($color-main, 0.1);
+            transition: opacity 170ms ease-in-out;
+        }
+
+        &:hover {
+            .text-wrapper {
+                opacity: 1;
+            }
+
+            .gallery-title {
+                opacity: 1;
+            }
+        }
+    }
+</style>
--- src/views/Gallery/Gallery.vue
+++ src/views/Gallery/Gallery.vue
@@ -1,18 +1,41 @@
 <template>
     <div class="gallery-wrapper">
-        <router-link :to="{ name: 'GalleryPage', params: { testString: 'This is a test string', id: 1 } }" exact>
-            Link
-        </router-link>
+        <GalleryCard
+            v-for="gallery in galleries"
+            :key="gallery.id"
+            :url="gallery.id"
+            :title="gallery.title"
+            :image="`/images/gallery/${gallery.id}/${gallery.displayImage}`"
+        />
     </div>
 </template>
 
 <script>
+    import galleryList from "@/assets/lists/galleryList.json";
+    import GalleryCard from "@/components/GalleryCard";
+
     export default {
         name: "Gallery",
         metaInfo: {
             title: "Vanessa | Gallery"
+        },
+        data() {
+            return {
+                galleries: galleryList
+            };
+        },
+        components: {
+            GalleryCard
         }
     };
 </script>
 
-<style lang="scss"></style>
+<style lang="scss">
+    .gallery-wrapper {
+        display: flex;
+        flex: 1 0 auto;
+        justify-content: space-evenly;
+        align-items: center;
+        flex-flow: row wrap;
+    }
+</style>
